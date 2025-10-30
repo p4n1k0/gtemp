1de87598c7c132c4d6523eb6a46bb0b6607b0c15
p4n1k0 <gabre.tarick@gmail.com>
2025-10-30 15:59:02 -0300

    chama endpoint de pesquisa de cidades com o termo de busca e exibe um alert caso lista vazia

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,28 @@
-// Remova os comentários a medida que for implementando as funções
+export const searchCities = async (term) => {
+  const token = import.meta.env.VITE_TOKEN;
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/search.json?lang=pt&key=${token}&q=${encodeURIComponent(term)}`,
+    );
+
+    if (!response.ok) {
+      throw new Error('Erro ao buscar cidades');
+    }
+
+    const data = await response.json();
+
+    if (data.length === 0) {
+      window.alert('Nenhuma cidade encontrada');
+      return [];
+    }
+
+    return data;
+  } catch (error) {
+    console.error('Erro:', error);
+    window.alert('Erro ao buscar cidades. Tente novamente mais tarde.');
+    return [];
+  }
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
